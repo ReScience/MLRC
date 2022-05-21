73f02463433c071eb7004804b259737bb04b132b
Koustuv Sinha <koustuvsinha@hotmail.com>
2022-05-21 17:58:32 -0400

    complete editorial and build for ahmed2022re/

---
+++ 2021/ahmed2022re/openreview/.auctex-auto/packages.el
@@ -0,0 +1,31 @@
+(TeX-add-style-hook
+ "packages"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "inputenc"
+    "hyperref"
+    "url"
+    "booktabs"
+    "amsfonts"
+    "nicefrac"
+    "microtype"
+    "amsmath"
+    "caption"
+    "float"
+    "graphicx"
+    "subcaption"
+    "graphics"
+    "ulem"
+    "blindtext"))
+ :latex)
+
