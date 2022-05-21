eaa42f3574b4656203eb6cac931965ac289cbaa4
Koustuv Sinha <koustuvsinha@hotmail.com>
2022-05-21 18:02:51 -0400

    complete editorial and build for eaton2022reproduction/

---
+++ 2021/eaton2022reproduction/journal/.auctex-auto/bibliography.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "bibliography"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Sinha:2021"
+    "dinan2020"
+    "miller2018"
+    "roller2020"
+    "urbanek2019"
+    "humeau2020"
+    "dinan2019second"
+    "devlin-etal-2019-bert"
+    "gpt2"
+    "gcp2021"
+    "redditmodel"
+    "parlgenderation"
+    "zhao-etal-2018-learning"
+    "celikyilmaz2020evaluation"
+    "parlai-colab"
+    "mesgar2018"))
+ :bibtex)
+
