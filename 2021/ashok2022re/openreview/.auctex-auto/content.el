b1db04518a46fb519f6695df11e42a9e35af9b0c
Koustuv Sinha <koustuvsinha@hotmail.com>
2022-05-21 17:59:19 -0400

    complete editorial and build for ashok2022re/

---
+++ 2021/ashok2022re/openreview/.auctex-auto/content.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook
+ "content"
+ (lambda ()
+   (LaTeX-add-labels
+    "sec:claims"
+    "ss:comp_req"
+    "sec:results"
+    "fig:Res-18-small"
+    "table:mini_imagenet"
+    "table:resnet_few_shot"
+    "fig:20-40-60-80"
+    "fig:20-60-other"
+    "fig:lowres"
+    "subsubsec:conv4"
+    "table:conv_rotation"
+    "table:conv_jigsaw"
+    "fig:domain"
+    "fig:Conv4"
+    "table:cdfsl_resnet"))
+ :latex)
+
